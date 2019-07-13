4c16e5bab02832b30600ee37aa3d69293228066c
Cash TingHin Lo <cash.raistlin@gmail.com>
2019-07-13 21:47:08 +0200

    Update gb_printer_emulator.ino
    Add drawing

--- gb_printer_emulator.ino
+++ gb_printer_emulator.ino
@@ -15,9 +15,6 @@ const int packet_byte_checksum_2  = 8;
 const int packet_byte_alive       = 9;
 const int packet_byte_status      = 10;
 
-
-
-
 byte serial_data;
 int data_counter = 8;
 int line;
@@ -36,6 +33,39 @@ void print_status(byte input_byte){
       GO.lcd.print(output);
       line++;
   }
+
+int draw_pos;
+uint32_t palette[] = {0x000000FF, 0x000000AF, 0x0000006F, 0x00000000};
+byte print_data[6400];
+int print_data_index;
+bool drawing_print_data = false;
+
+void draw_data() {
+  int i = 0;
+  while (i < print_data_index-1){
+    byte byte_1 = print_data[i];
+    byte byte_2 = print_data[i+1];
+    int tile_col = (i/2/8)%20;
+    int tile_row = i/(20*2*8);
+
+    int y_tile_pos = i/2%8;
+    
+    int j = 7;
+    while ( j >= 0 ){
+        int x_tile_pos = j;
+        
+        uint32_t pixel_color = palette[byte_1&1 + (byte_2&1)*2];
+        byte_1 >>= 1;
+        byte_2 >>= 1;
+        GO.lcd.drawRect((x_tile_pos+tile_col*8)*2, (y_tile_pos+tile_row*8)*2, 2, 2, pixel_color);
+        j--;
+    }
+    i += 2;
+  }
+  drawing_print_data = false;
+  print_data_index = 0;
+}
+
 
 void process_byte(byte input_byte) {
   switch (awaiting_byte) {
@@ -65,7 +95,8 @@ void process_byte(byte input_byte) {
       }
       break;
     case packet_byte_data:
-      //TODO
+      print_data[print_data_index] = input_byte;
+      print_data_index++;
       data_length_left--;
       if (data_length_left == 0) {
           break;
@@ -83,7 +114,8 @@ void process_byte(byte input_byte) {
       break;
     case packet_byte_status:
       awaiting_byte = packet_byte_magic_1;
-      print_status(input_byte);
+      if (command == 0x2) drawing_print_data = true;
+      //print_status(input_byte);
       return;
   }
   awaiting_byte++;
@@ -126,4 +158,5 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  if(drawing_print_data) draw_data();
 }
